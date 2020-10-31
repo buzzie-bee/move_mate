1e15371774f95573d0f211ca163e5266be5bd5c7
buzzie-bee <39500959+buzzie-bee@users.noreply.github.com>
2020-10-31 19:02:12 +0000

    Started implementing navigation and drawers

--- lib/main.dart
+++ lib/main.dart
@@ -3,66 +3,18 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:move_mate/screens/login_screen.dart';
+import 'package:move_mate/screens/my_moves.dart';
+import 'package:move_mate/screens/settings.dart';
 import 'package:move_mate/screens/welcome_loading.dart';
 import 'package:move_mate/screens/welcome_error.dart';
 import 'package:move_mate/screens/home_screen.dart';
+import 'package:move_mate/widgets/navigation_drawer.dart';
+import 'package:move_mate/screens/profile.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
-
-// class MyApp extends StatefulWidget {
-//   _MyAppState createState() => _MyAppState();
-// }
-//
-// class _MyAppState extends State<MyApp> {
-//   // Set default `_initialized` and `_error` state to false
-//   bool _initialized = false;
-//   bool _error = false;
-//
-//   // Define an async function to initialize FlutterFire
-//   void initializeFlutterFire() async {
-//     try {
-//       // Wait for Firebase to initialize and set `_initialized` state to true
-//       await Firebase.initializeApp();
-//       setState(() {
-//         _initialized = true;
-//         Navigator.pushNamed(context, LoginScreen.id);
-//       });
-//     } catch (e) {
-//       // Set `_error` state to true if Firebase initialization fails
-//       setState(() {
-//         _error = true;
-//         Navigator.pushNamed(context, WelcomeError.id);
-//       });
-//     }
-//   }
-//
-//   String initialRoute() {
-//     return _initialized ? LoginScreen.id : WelcomeLoading.id;
-//   }
-//
-//   @override
-//   void initState() {
-//     initializeFlutterFire();
-//     super.initState();
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     // Show error message if initialization failed
-//     return MaterialApp(
-//       //initialRoute: initialRoute(),
-//       routes: {
-//         WelcomeLoading.id: (context) => WelcomeLoading(),
-//         WelcomeError.id: (context) => WelcomeError(),
-//         LoginScreen.id: (context) => LoginScreen(),
-//       },
-//       builder: ,
-//     );
-//   }
-// }
 
 class MyApp extends StatelessWidget {
   // Create the initialization Future outside of `build`:
@@ -71,28 +23,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      //initialRoute: initialRoute(),
       routes: {
         WelcomeLoading.id: (context) => WelcomeLoading(),
         WelcomeError.id: (context) => WelcomeError(),
         LoginScreen.id: (context) => LoginScreen(),
+        HomeScreen.id: (context) => HomeScreen(),
+        Profile.id: (context) => Profile(),
+        Settings.id: (context) => Settings(),
+        MyMoves.id: (context) => MyMoves(),
       },
-      home: FutureBuilder(
-        future: _initialization,
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return WelcomeError();
-          }
-          if (snapshot.connectionState == ConnectionState.done) {
-            return StreamBuilder<User>(
-              stream: FirebaseAuth.instance.authStateChanges(),
-              builder: (BuildContext context, snapshot) {
-                return (!snapshot.hasData) ? LoginScreen() : HomeScreen();
-              },
-            );
-          }
-          return WelcomeLoading();
-        },
+      theme: ThemeData.light(),
+      home: SafeArea(
+        child: FutureBuilder(
+          future: _initialization,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return WelcomeError();
+            }
+            if (snapshot.connectionState == ConnectionState.done) {
+              return StreamBuilder<User>(
+                stream: FirebaseAuth.instance.authStateChanges(),
+                builder: (BuildContext context, snapshot) {
+                  return (!snapshot.hasData) ? LoginScreen() : HomeScreen();
+                },
+              );
+            }
+            return WelcomeLoading();
+          },
+        ),
       ),
     );
   }
